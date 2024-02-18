beec4aff723be6e299038470a8c28d6b6241e948
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-18 21:59:44 +0530

    Login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="absolute py-2 px-8 bg-gradient-to-b from-black">
+    <div className="absolute py-2 px-8 bg-gradient-to-b from-black z-10">
       <img
         className="w-44"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,12 +4,30 @@ const Login = () => {
   return (
     <>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt=""
         />
       </div>
+      <form className="w-4/12 absolute px-12 bg-black my-32 mx-auto right-0 left-0  text-white bg-opacity-80 rounded-md">
+        <h1 className="text-3xl font-bold py-4 px-2 rounded-lg">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email or phone number"
+          className="p-4 my-4 w-full rounded-lg"
+          style={{ backgroundColor: "rgb(51,51,51)" }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full rounded-lg"
+          style={{ backgroundColor: "rgb(51,51,51)" }}
+        />
+        <button className="p-4 my-4 bg-red-500 text-white w-full rounded-lg">
+          Sign In
+        </button>
+      </form>
     </>
   );
 };
