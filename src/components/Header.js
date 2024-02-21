d14707e70dc3d2b1169d5f23e8fa6c1963731f86
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-21 18:35:45 +0530

    Unsubscribed to the onAuthStateChanged callback

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,14 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in or sign up
         const { uid, email, displayName, photoURL } = user;
@@ -31,6 +32,8 @@ const Header = () => {
         navigate("/");
       }
     });
+    // Unsiubscribe when component unmounts
+    return () => unsubscribe();
   }, []);
   const handleSignOut = () => {
     signOut(auth)
@@ -45,11 +48,7 @@ const Header = () => {
   return (
     <>
       <div className="w-full absolute py-2 px-8 bg-gradient-to-b from-black z-10 flex justify-between">
-        <img
-          className="w-44"
-          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-          alt="Logo"
-        />
+        <img className="w-44" src={LOGO} alt="Logo" />
         {user && (
           <div className="p-2 z-10 flex right-0">
             <img className="w-12 h-12" src={user?.photoURL} alt="userImage" />
