5d3426535db1dac9a7a4793ba716adfc2966bfed
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-22 17:16:19 +0530

    Dispatching action to store movies in the store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,15 +1,19 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
     );
     const json = await data.json();
-    console.log(json);
+    console.log(json?.results);
+    dispatch(addNowPlayingMovies(json?.results));
   };
   useEffect(() => {
     getNowPlayingMovies();
