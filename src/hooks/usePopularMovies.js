471d68d6776f2206985d15ac2a640c60cac3b6e1
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-23 22:18:57 +0530

    usePopularMovies Custom Hook

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addPopularPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const usePopularMovies = () => {
+  //Fetch data from TMDB website and store in the redux-store
+  const dispatch = useDispatch();
+  const getPopularPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    dispatch(addPopularPlayingMovies(json?.results));
+  };
+  useEffect(() => {
+    getPopularPlayingMovies();
+  }, []);
+};
+export default usePopularMovies;
