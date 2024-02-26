6ae250ab144512e699fd67c6d53612a36c7926f3
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-26 15:02:18 +0530

    Popular movies API call memoization

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addPopularPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 const usePopularMovies = () => {
   //Fetch data from TMDB website and store in the redux-store
   const dispatch = useDispatch();
+  const popularMovies = useSelector((store) => store.movies.popularMovies);
   const getPopularPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
@@ -15,7 +16,7 @@ const usePopularMovies = () => {
     dispatch(addPopularPlayingMovies(json?.results));
   };
   useEffect(() => {
-    getPopularPlayingMovies();
+    !popularMovies && getPopularPlayingMovies();
   }, []);
 };
 export default usePopularMovies;
