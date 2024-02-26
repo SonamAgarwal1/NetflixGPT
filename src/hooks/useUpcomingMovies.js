e979e7899a8e9f9554aa7e860c9f8d3398059381
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-26 15:03:03 +0530

    Upcoming movies API call memoization

--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addUpcomingPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 const useUpcomingMovies = () => {
   //Fetch data from TMDB website and store in the redux-store
   const dispatch = useDispatch();
+  const upcomingMovies = useSelector((store) => store.movies.upcomingMovies);
   const getUpcomingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
@@ -15,7 +16,7 @@ const useUpcomingMovies = () => {
     dispatch(addUpcomingPlayingMovies(json?.results));
   };
   useEffect(() => {
-    getUpcomingMovies();
+    !upcomingMovies && getUpcomingMovies();
   }, []);
 };
 export default useUpcomingMovies;
