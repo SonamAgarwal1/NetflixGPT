934287427081c2d9e8293891854bfcf27b248086
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-26 15:02:36 +0530

    Top rated movies API call memoization

--- src/hooks/useTopRatedMovies.js
+++ src/hooks/useTopRatedMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTopRatedPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 const useTopRatedMovies = () => {
   //Fetch data from TMDB website and store in the redux-store
   const dispatch = useDispatch();
+  const topRatedMovies = useSelector((store) => store.movies.topRatedMovies);
+
   const getTopRatedMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
@@ -15,7 +17,7 @@ const useTopRatedMovies = () => {
     dispatch(addTopRatedPlayingMovies(json?.results));
   };
   useEffect(() => {
-    getTopRatedMovies();
+    !topRatedMovies && getTopRatedMovies();
   }, []);
 };
 export default useTopRatedMovies;
