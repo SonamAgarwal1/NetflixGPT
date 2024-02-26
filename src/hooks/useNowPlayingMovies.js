ea97b2d363e09abd9cd8a719c5168b14479ce9f9
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-02-26 15:01:55 +0530

    Now Playing movies API call memoization

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS, NOW_PLAYING_MOVIE } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,13 +6,16 @@ import { useEffect } from "react";
 const useNowPlayingMovies = () => {
   //Fetch data from TMDB website and store in the redux-store
   const dispatch = useDispatch();
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
   const getNowPlayingMovies = async () => {
     const data = await fetch(NOW_PLAYING_MOVIE, API_OPTIONS);
     const json = await data.json();
     dispatch(addNowPlayingMovies(json?.results));
   };
   useEffect(() => {
-    getNowPlayingMovies();
+    !nowPlayingMovies && getNowPlayingMovies();
   }, []);
 };
 export default useNowPlayingMovies;
